0ff1f36106b6a88f5877ae55db61b2262cdd33ea
Sergio Costa <sergio.costa@ufma.br>
2025-05-09 17:54:04 -0300

    incluído exemplo

---
+++ docs/examples/game_of_life_streamlit_random.py
@@ -0,0 +1,90 @@
+
+from dissmodel.core import Model, Environment, RegularGrid
+
+from dissmodel.visualization.map import Map
+
+from dissmodel.visualization.streamlit import StreamlitMap
+
+
+from matplotlib.colors import ListedColormap
+
+import streamlit as st
+
+class GameOfLife(Model):
+       
+
+    def rule(self, idx):
+        """
+        Define a regra do Game of Life para atualizar o estado de uma célula.
+        """
+        # Estado atual da célula
+        value = self.env.gdf.loc[idx].state
+        
+        # Estados dos vizinhos
+        neighs = self.neighs(idx)
+        count = neighs["state"].sum()
+        
+        # Aplicar as regras do Game of Life
+        if value == 1:  # Célula viva
+            if count < 2 or count > 3:  # Subpopulação ou superpopulação
+                return 0  # Morre
+            else:
+                return 1  # Sobrevive
+        else:  # Célula morta
+            if count == 3:  # Reprodução
+                return 1  # Revive
+            else:
+                return 0  # Continua morta
+            
+    def execute(self):
+        # Aplicar a função `rule` a todos os índices e armazenar os novos estados
+        self.env.gdf["state"] = self.env.gdf.index.map(self.rule)
+        print (self.env.now())
+
+
+
+
+
+## a configuracao começaria aqui
+
+# Configurações da aplicação
+st.set_page_config(page_title="Modelo Game of Life", layout="centered")
+st.title("Simulação com Game of Life (DisSModel)")
+
+# Parâmetros do usuário
+steps = st.slider("Número de passos da simulação", min_value=1, max_value=50, value=10)
+grid_dim = st.slider("Tamanho da grade", min_value=5, max_value=50, value=20)
+
+custom_cmap = ListedColormap(['green', 'red'])
+plot_params={ "column": "state","cmap": custom_cmap,  "ec" : 'black'}
+
+
+import numpy as np
+
+# Inicializar estado da sessão
+if st.button("Executar Simulação"):
+
+    # Área de plotagem reservada
+    plot_area = st.empty()
+
+    ### espaço 
+    grid = RegularGrid(bounds=(0, 0, 100, 100), dim=grid_dim, attrs={'state': 0})
+    gdf = grid.to_geodaframe()
+    n = len(gdf)
+    gdf["state"] = np.random.choice([0, 1], size=n, p=[0.7, 0.3])
+
+    env = Environment (
+        gdf = grid.to_geodaframe(),
+        end_time = steps,
+        start_time=0
+    )
+
+
+    GameOfLife(create_neighbohood="Queen")
+
+    StreamlitMap(  
+        plot_area = plot_area,
+        plot_params={ "column": "state","cmap": custom_cmap,  "ec" : 'black'}
+    )
+
+    env.run()
