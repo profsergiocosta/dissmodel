de66ecaefb278c9f2605f2942eb74234901b6641
Sergio Costa <sergio.costa@ufma.br>
2025-06-08 09:25:21 -0300

    update

--- dissmodel/visualization/map.py
+++ dissmodel/visualization/map.py
@@ -1,9 +1,14 @@
-
 import matplotlib.pyplot as plt
-
-
 from dissmodel.core import Model
 
+# Função auxiliar para detectar se está rodando em notebook
+def is_notebook():
+    try:
+        from IPython import get_ipython
+        shell = get_ipython().__class__.__name__
+        return shell == "ZMQInteractiveShell"
+    except Exception:
+        return False
 
 
 class Map(Model):
@@ -15,19 +20,36 @@ class Map(Model):
         self.plot_area = plot_area
 
     def update(self, year, gdf):
+
+        if is_notebook():
+            from IPython.display import clear_output
+            clear_output(wait=True)  # Limpa a saída do notebook para exibir apenas o gráfico atualizado
+
         self.ax.clear()
         self.gdf.plot(ax=self.ax, **self.plot_params)
-        self.ax.set_title(f'Map for {year}')
+        self.ax.set_title(f'Mapa - Ano {year}')
+        plt.tight_layout()
         plt.draw()
 
         if self.plot_area:
-            self.plot_area.pyplot(self.fig)  # Streamlit
+            # Modo Streamlit
+            self.plot_area.pyplot(self.fig)
+        elif is_notebook():
+            # Modo Jupyter Notebook
+            from IPython.display import display
+            #from IPython.display import clear_output
+            #clear_output(wait=True)  # Limpa a saída do notebook para exibir apenas o gráfico atualizado
+            display(self.fig)    
+            #plt.show()        
+            plt.close(self.fig)
+            #plt.draw()  # Desenha o gráfico na tela
         elif self.pause:
+            # Modo terminal interativo
             plt.pause(0.01)
             if self.env.now() == self.env.end_time:
-                plt.show()  # Modo interativo local
+                plt.show()
+
 
     def execute(self):
         year = self.env.now()
         self.update(year, self.gdf)
-
